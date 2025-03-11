c01d8869960c37a2164f0716e144a03d8a207603
Gustavo Duara <gugaduara@gmail.com>
2025-03-11 17:16:25 -0300

    Add project files.

---
+++ jogo adivinhação/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace jogo_adivinhação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // loop principal do jogo
+            while (true) ;
+
+
+            Console.Clear();
+            Console.WriteLine("____________");
+            Console.WriteLine("Jogo da adivinhação");
+            Console.WriteLine("____________");
+
+            //Escolha de dificuldade
+            Console.WriteLine("Escolha um nível de dificuldade:");
+            Console.WriteLine("_________________________________");
+            Console.WriteLine(
+           
+
+
+        }
+    }
+}
